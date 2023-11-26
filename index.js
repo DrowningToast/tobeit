b065dd3d7e96709f903a7b37307e49d87e11fece
DrowningToast <supratouchgussii@gmail.com>
2023-11-26 11:43:42 +0700

    add: image

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.port ?? 3000; //
 const TOKEN = process.env.access;
 
-const { LineClient } = require("messaging-api-line");
+const { LineClient, Line } = require("messaging-api-line");
 
 const client = new LineClient({
   accessToken: TOKEN,
@@ -35,12 +35,21 @@ app.post("/webhook", (req, res) => {
     const text = req.body.events[0].message.text; // ข้อความที่ส่งมา
     const replyToken = req.body.events[0].replyToken; // replyToken ที่ Line ส่งมา
 
-    client.reply(replyToken, [
-      {
-        type: "text",
-        text: "Hello! you just send the message " + text,
-      },
-    ]);
+    if (text === "image") {
+      client.reply(replyToken, [
+        Line.createText("Hello! you just send the message " + text),
+        Line.createImage({
+          originalContentUrl: "https://tobeit.it20.dev/logo-about.webp",
+          previewImageUrl: "https://tobeit.it20.dev/logo-about.webp",
+        }),
+      ]);
+    } else if (text === "text") {
+      client.reply(replyToken, [
+        Line.createText("Hello! you just send the message " + text),
+      ]);
+    } else {
+      client.reply(replyToken, [Line.createText("Unknown keyword!")]);
+    }
   } else {
     res.send("Unknown action");
   }
